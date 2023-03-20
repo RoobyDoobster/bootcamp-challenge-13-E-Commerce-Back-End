e4cbf55feda0aa8293d39e8c17a488d9a976d50a
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-03-19 17:58:15 -0700

    added get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const data = await Category.findAll({
+      include: { model: Product },
+    });
+    if(!data) {
+      res.status(404).json({message: 'No Category with this id'});
+      return;
+    }
+    res.status(200).json(data);
+  } catch(error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const data = await Category.findById(req.params.id, {
+      include: {model: Product },
+    });
+    if(!data) {
+      res.status(404).json({ message: 'No Category with this id'});
+      return;
+    }
+    res.status(200).json(data);
+  } catch(error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post('/', (req, res) => {
